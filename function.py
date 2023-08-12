892b15828a7b8f14a8c2fdac6033e40ecd273bdc
duchengli <duchengli@hotmail.com>
2023-08-12 16:01:45 +0800

    20230812@homev3

--- function.py
+++ function.py
@@ -1,21 +1,37 @@
 from mootdx import consts
 from mootdx.quotes import Quotes
+from datetime import datetime, timedelta
+
+client = Quotes.factory(market="std")
+
 
 def get_stocks():
     stocks = []
     client = Quotes.factory(market="std")
     symbol = client.stocks(market=consts.MARKET_SH)
     for item in symbol["code"].to_list():
-        if item[:3] in ['600','601','603','605','688']:
+        if item[:3] in ["600", "601", "603", "605", "688"]:  # 查询沪市主板和科创板股票代码
             stocks.append(item)
 
     symbol = client.stocks(market=consts.MARKET_SZ)
     for item in symbol["code"].to_list():
-        if item[:3] in ['000','001','002','003','300','301']:
+        if item[:3] in ["000", "001", "002", "003", "300", "301"]:
             stocks.append(item)
-    
+
     return stocks
 
-my_stocks = get_stocks()
-print(2236+2815)
-print(len(my_stocks))
+
+def get_zongshizhi(stock="000001", delay=0):
+    tdate = datetime.now() - timedelta(delay)
+    tdate_str = tdate.strftime("%Y/%-m/%-d") + " 15:00"  # windows平台下运行用# linux下用-
+    zgb = client.finance(symbol=stock).loc[0, "zongguben"]
+    # price = client.bars(symbol=stock, adjust="qfq", frequency=9, offset=1, start=0).loc[
+    #     "2023/8/11 15:00", "close"
+    # ]
+    price = client.bars(symbol=stock, adjust="qfq", frequency=9, offset=1, start=0).loc[
+        tdate_str, "close"
+    ]
+    return round(zgb * price / 100000000, 2)
+
+
+print(get_zongshizhi("000001", 1))
--- main.py
+++ main.py
@@ -1,43 +1,5 @@
-import pandas as pd
-from matplotlib import pyplot
-from mootdx.contrib.adjust import get_adjust_year
-from statsmodels.tsa.seasonal import seasonal_decompose
+import function as FN
 
-df2013 = get_adjust_year(symbol="000001", year="2013", factor="01")
-df2014 = get_adjust_year(symbol="000001", year="2014", factor="01")
-df2015 = get_adjust_year(symbol="000001", year="2015", factor="01")
-df2016 = get_adjust_year(symbol="000001", year="2016", factor="01")
-df2017 = get_adjust_year(symbol="000001", year="2017", factor="01")
-df2018 = get_adjust_year(symbol="000001", year="2018", factor="01")
-df2019 = get_adjust_year(symbol="000001", year="2019", factor="01")
-df2020 = get_adjust_year(symbol="000001", year="2020", factor="01")
-df2021 = get_adjust_year(symbol="000001", year="2021", factor="01")
-df2022 = get_adjust_year(symbol="000001", year="2022", factor="01")
-df2023 = get_adjust_year(symbol="000001", year="2023", factor="01")
-
-df = pd.concat(
-    [
-        df2013,
-        df2014,
-        df2015,
-        df2016,
-        df2017,
-        df2018,
-        df2019,
-        df2020,
-        df2021,
-        df2022,
-        df2023,
-    ],
-    # ignore_index=True
-)
-print(df.info())
-
-
-# result = seasonal_decompose(df["close"], model="additive")
-# # result.plot()
-# # pyplot.show()
-# print(result.trend)
-# print(result.seasonal)
-# print(result.resid)
-# print(result.observed)
+stocks_list = FN.get_stocks()
+# print(stocks_list)
+print(FN.get_zongshizhi("600300"))
