dfa892560491634f98a0d556e07167a792c93944
duchengli <duchengli@hotmail.com>
2023-08-12 23:33:46 +0800

    20230812@homev5

--- function.py
+++ function.py
@@ -12,26 +12,27 @@ def get_stocks():
     for item in symbol["code"].to_list():
         if item[:3] in ["600", "601", "603", "605", "688"]:  # 查询沪市主板和科创板股票代码
             stocks.append(item)
-
-    symbol = client.stocks(market=consts.MARKET_SZ)
+    symbol = client.stocks(market=consts.MARKET_SZ)  # 查询深市主板和创业板股票代码
     for item in symbol["code"].to_list():
         if item[:3] in ["000", "001", "002", "003", "300", "301"]:
             stocks.append(item)
-
     return stocks
 
 
 def get_zongshizhi(stock="000001", delay=0):
     tdate = datetime.now() - timedelta(delay)
     tdate_str = tdate.strftime("%Y/%-m/%-d") + " 15:00"  # windows平台下运行用# linux下用-
-    zgb = client.finance(symbol=stock).loc[0, "zongguben"]
+    zgb = client.finance(symbol=stock).loc[0, "zongguben"]  # 获取总股本
     # price = client.bars(symbol=stock, adjust="qfq", frequency=9, offset=1, start=0).loc[
     #     "2023/8/11 15:00", "close"
     # ]
-    price = client.bars(symbol=stock, adjust="qfq", frequency=9, offset=1, start=0).loc[
-        tdate_str, "close"
-    ]
-    return round(zgb * price / 100000000, 2)
+    try:  # 获取收盘股价
+        price = client.bars(
+            symbol=stock, adjust="qfq", frequency=9, offset=1, start=0
+        ).loc[tdate_str, "close"]
+        return round(zgb * price / 100000000, 2)
+    except Exception as unkown_error:
+        print(unkown_error)
 
 
-print(get_zongshizhi("000001", 1))
+# print(get_zongshizhi("601236", 1))
--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import function as FN
+import time
 
 stocks_list = FN.get_stocks()
 # print(stocks_list)
-print(FN.get_zongshizhi("600300"))
+# print(FN.get_zongshizhi("600300", 1))
+for stock in stocks_list:
+    time.sleep(2)
+    print(stock + ": " + str(FN.get_zongshizhi(stock, 1)))
